cf39d202db84cf2f72e91574744d6432a677cac0
Hadrien Croubois <hc@iex.ec>
2020-08-27 11:29:42 +0200

    testing create2 factory: working!

--- test/000.testing.spec.js
+++ test/000.testing.spec.js
@@ -1,4 +1,5 @@
 const Testing = require('../build/Testing.json')
+const FACTORY = require('@iexec/solidity/deployment/factory.json')
 
 const { use, expect } = require('chai')
 const { solidity, deployContract } = require('ethereum-waffle')
@@ -19,19 +20,44 @@ describe('EVM/OVM features', () => {
 	before(async () => {
 		provider = await getProvider()
 		wallets  = provider.getWallets()
-
-		Instance = await deployContract(wallets[0], Testing, [])
+		// Instance = await deployContract(wallets[0], Testing, [])
 	})
 
 	beforeEach(async () => {})
 
 	// Test
+	describe('EIP-155', async () => {
+		it ('factory deployment', async () => {
+			// Only EVM: fill the deploying wallet
+			if (process.env.MODE === 'EVM') {
+				await wallets[0].sendTransaction({ to: FACTORY.deployer, value: 5194156000000000 })
+			}
+
+			// Deploy factory
+			await provider.send('eth_sendRawTransaction', [ FACTORY.tx ])
+
+			// checks
+			expect(ethers.utils.keccak256(await provider.send('eth_getCode', [ FACTORY.address ]))).to.equal('0xa8ca8e2cb0b841a4239cea8886b1c912ac194ebb0e953e5ac2710dc8d0b083c7')
+		})
+
+		it ('factory usage', async () => {
+			const factory  = new ethers.Contract(FACTORY.address, FACTORY.abi, wallets[0])
+			const salt     = ethers.utils.randomBytes(32)
+			const expected = await factory.predictAddress('0x'+Testing.bytecode, salt)
+
+			await expect(factory.createContract('0x'+Testing.bytecode, salt)).to.emit(factory, 'NewContract').withArgs(expected)
+
+			Instance = new ethers.Contract(expected, Testing.abi, wallets[0])
+		})
+	})
+
 	describe('precompiles', async () => {
 		it('recover', async () => {
 			const signer    = new ethers.utils.SigningKey(ethers.utils.randomBytes(32))
 			const hash      = ethers.utils.randomBytes(32)
 			const signature = ethers.utils.joinSignature(signer.signDigest(ethers.utils.hashMessage(hash)))
+			
 			expect(await Instance.recover(hash, signature)).to.equal(signer.address)
-		});
+		})
 	})
 });
